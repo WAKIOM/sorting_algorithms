fbe3336700e43c08faea7c5a36902d7de0c8e397
WAKIOM <agnessmanga@gmail.com>
2023-11-17 17:45:21 +0300

    fix: update solution to task 0

--- sort.h
+++ sort.h
@@ -1,5 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
+#include <stdbool.h>
+#include <stddef.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -9,9 +11,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
