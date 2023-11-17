6c04bad6d36818f296d0bf243c8b83cfd509cb63
WAKIOM <agnessmanga@gmail.com>
2023-11-17 17:59:08 +0300

    fix: update answer to task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,9 +11,14 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int temp;
+	int swapped;
+
+	if (array == NULL || size < 2)
+		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
+		swapped = 0;
 		for (j = 0; j < size - 1 - i; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -21,8 +26,13 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+				swapped = 1;
 				print_array(array, size);
 			}
+		}
+		if (swapped == 0)
+		{
+			return;
 		}
 	}
 }
